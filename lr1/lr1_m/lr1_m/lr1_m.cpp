1dbbdab97d20ea4ac727b6748ae8a1f93a642cff
Arslan Rafikov <rafikov.ai@gazprom-neft.ru>
2023-12-14 00:30:20 +0500

    правки в 1 лабе

--- lr1/lr1_m/lr1_m/lr1_m.cpp
+++ lr1/lr1_m/lr1_m/lr1_m.cpp
@@ -5,18 +5,21 @@
 using namespace cv;
 using namespace std;
 
-void gauss_kernel(Mat&, int, float);
-void gauss_bluray(const Mat&, Mat&, int, float);
-void gauss_contour(const Mat&, Mat&, int, float);
-void gauss_sharpness(const Mat&, Mat&, int, float, float);
-void difference_of_gaussians(const Mat&, Mat&);
+void differenceOfGaussians(const Mat&, Mat&);
 void roberts(const Mat&, Mat& );
 
-void insertion_sort(int window[]);
-void median_blure(Mat& , Mat& );
+void insertionSort(int window[]);
+void medianBlure(Mat& , Mat& );
 void mosaic(const Mat&, Mat&);
 
-void show_image(const Mat, Mat&, string);
+void showImage(const Mat, Mat&, string);
+
+
+void applyGaussianBlur(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize, double sigma);
+void calculateGradient(const cv::Mat& inputImage, cv::Mat& gradientX, cv::Mat& gradientY);
+void applyGaussianContour(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize, double sigma);
+void applySharpening(const cv::Mat& inputImage, cv::Mat& outputImage);
+void applyDoG(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize1, double sigma1, int kernelSize2, double sigma2);
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -47,44 +50,70 @@ int main() {
         switch (input_key)
         {
             case 1:
-                gauss_bluray(input_image, output_image, 5, 1.0);
-                show_image(input_image, output_image, "Гаусс размытие");
+            {
+                applyGaussianBlur(input_image, output_image, 5, 1.0);
+                showImage(input_image, output_image, "Гаусс размытие");
                 break;
+            }
             case 2:
-                gauss_contour(input_image, output_image, 5, 1.0);
-                show_image(input_image, output_image, "Гаусс Контур");
-                break;
+            {
+                applyGaussianContour(input_image, output_image, 5, 1.0);
+                showImage(input_image, output_image, "Гаусс Контур");
+                break; 
+            }
             case 3:
-                gauss_sharpness(input_image, output_image, 5, 1.0, 0.35);
-                show_image(input_image, output_image, "Увеличение резкости");
+            {
+                applySharpening(  input_image, output_image);
+                showImage(input_image, output_image, "Увеличение резкости");
                 break;
+            }
             case 4:
-                difference_of_gaussians(input_image, DoG);            
-                output_image = DoG;
-                show_image(input_image, output_image, "Разность гауссианов"); 
+            {
+                cv::Mat dogImage;
+                int kernelSize1 = 5, kernelSize2 = 9;
+                double sigma1 = 1.0, sigma2 = 2.0;
+
+                applyDoG(input_image, dogImage, kernelSize1, sigma1, kernelSize2, sigma2);
+
+                //differenceOfGaussians(input_image, DoG);
+                //output_image = DoG;
+                showImage(input_image, dogImage, "Разность гауссианов");
                 break;
+            }
             case 5:
+            {
                 Canny(input_image, output_image, 50, 200);
-                show_image(input_image, output_image, "Фильтр Кэнни"); 
+                showImage(input_image, output_image, "Фильтр Кэнни");
                 break;
+            }
             case 6:
+            {
                 roberts(input_image, output_image);
-                show_image(input_image, output_image, "Оператор Робертса"); 
+                showImage(input_image, output_image, "Оператор Робертса");
                 break;
+            }
             case 7:
-                median_blure(input_image, output_image);
-                show_image(input_image, output_image, "Медианная фильтрация"); 
+            {
+                medianBlure(input_image, output_image);
+                showImage(input_image, output_image, "Медианная фильтрация");
                 break;
+            }
             case 8:
+            {
                 mosaic(input_image, output_image);
-                show_image(input_image, output_image, "Мозаика");
+                showImage(input_image, output_image, "Мозаика");
                 break;
+            }
             case 0:
+            {
                 cout << "Выход";
                 break;
+            }
             default:
-                cout << "Неверная команда\n";         
+            {
+                cout << "Неверная команда\n";
                 break;
+            }
         }
 
         if (input_key == 0) break;
@@ -92,7 +121,7 @@ int main() {
     return 0;
 }
 
-void show_image(const Mat input_img, Mat& output_img, string filter_name) {
+void showImage(const Mat input_img, Mat& output_img, string filter_name) {
     imshow("Исходное изображение", input_img);
     waitKey(0);
     imshow(filter_name, output_img);
@@ -101,110 +130,65 @@ void show_image(const Mat input_img, Mat& output_img, string filter_name) {
     imwrite("D:/фото/"+ filter_name +".jpg", output_img);
 }
 
-void gauss_kernel(Mat& kernel, int aperture_size, float sigma)
+void applySharpening(const cv::Mat& inputImage, cv::Mat& outputImage)
 {
-    float sum = 0.0;
-    for (int i = 0; i < aperture_size; i++) {
-        for (int j = 0; j < aperture_size; j++) {
-            float x = i - aperture_size / 2;
-            float y = j - aperture_size / 2;
-            kernel.at<float>(i, j) = exp(-(x * x + y * y) / (2 * sigma * sigma));
-            sum += kernel.at<float>(i, j);
-        }
-    }
-    kernel = kernel / sum;
-}
+    outputImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
 
-void gauss_bluray(const Mat& input_image, Mat& output_image, int aperture_size, float sigma)
-{   
-    Mat kernel(aperture_size, aperture_size, CV_32F);
-    gauss_kernel(kernel, aperture_size, sigma);
+    // Ядро Лапласиана для увеличения резкости
+    int kernelSize = 3;
+    float kernelData[] = { 0, -1, 0, -1, 5, -1, 0, -1, 0 };
+    cv::Mat kernel = cv::Mat(kernelSize, kernelSize, CV_32F, kernelData);
 
-    //// Применение фильтра Гаусса к изображению 
-    for (int i = aperture_size / 2; i < input_image.rows - aperture_size / 2; i++) {
-        for (int j = aperture_size / 2; j < input_image.cols - aperture_size / 2; j++) {
-
+    // Применяем ядро Лапласиана вручную
+    for (int y = 1; y < inputImage.rows - 1; ++y) {
+        for (int x = 1; x < inputImage.cols - 1; ++x) {
             float sum = 0.0;
-
-            for (int x = -aperture_size / 2; x <= aperture_size / 2; x++) {
-                for (int y = -aperture_size / 2; y <= aperture_size / 2; y++) {
-                    sum += kernel.at<float>(x + aperture_size / 2, y + aperture_size / 2) * input_image.at<uchar>(i + x, j + y);
+            for (int ky = -1; ky <= 1; ++ky) {
+                for (int kx = -1; kx <= 1; ++kx) {
+                    sum += kernel.at<float>(ky + 1, kx + 1) * inputImage.at<uchar>(y + ky, x + kx);
                 }
             }
-
-            output_image.at<uchar>(i, j) = sum;
+            outputImage.at<uchar>(y, x) = cv::saturate_cast<uchar>(sum);
         }
     }
 }
 
-void gauss_contour(const Mat& input_image, Mat& output_image, int aperture_size, float sigma)
+//void differenceOfGaussians(const Mat& input_image, Mat& output_image)
+//{
+//
+//    Mat blurred_image3x3(input_image.rows, input_image.cols, CV_8UC1);
+//    Mat blurred_image5x5(input_image.rows, input_image.cols, CV_8UC1);
+//
+//    applyGaussianBlur(input_image, blurred_image5x5, 5, 3.0);
+//
+//    for (int i = 0; i < input_image.rows; i++)
+//    {
+//        for (int j = 0; j < input_image.cols; j++)
+//        {
+//            int diff = blurred_image5x5.at<uchar>(i, j) - blurred_image3x3.at<uchar>(i, j);
+//            output_image.at<uchar>(i, j) = abs(diff);
+//        }
+//    }
+//}
+
+void applyDoG(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize1, double sigma1, int kernelSize2, double sigma2)
 {
-    output_image = input_image.clone();
+    cv::Mat blurredImage1, blurredImage2;
 
-    Mat kernel(aperture_size, aperture_size, CV_32F);
-    gauss_kernel(kernel, aperture_size, sigma);
-    
+    // Применяем первое размытие Гауссиана
+    applyGaussianBlur(inputImage, blurredImage1, kernelSize1, sigma1);
 
-    // Применение фильтра контуров к изображению  
-    for (int i = aperture_size / 2; i < input_image.rows - aperture_size / 2; i++) {
-        for (int j = aperture_size / 2; j < input_image.cols - aperture_size / 2; j++) {
+    // Применяем второе размытие Гауссиана
+    applyGaussianBlur(inputImage, blurredImage2, kernelSize2, sigma2);
 
-            float sum_x = 0.0;
-            float sum_y = 0.0;
+    // Вычисляем разность Гауссиан
+    outputImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
 
-            for (int x = -aperture_size / 2; x <= aperture_size / 2; x++) {
-                for (int y = -aperture_size / 2; y <= aperture_size / 2; y++) {
-                    sum_x += kernel.at<float>(x + aperture_size / 2, y + aperture_size / 2) * input_image.at<uchar>(i + x, j + y) * x;
-                    sum_y += kernel.at<float>(x + aperture_size / 2, y + aperture_size / 2) * input_image.at<uchar>(i + x, j + y) * y;
-                }
-            }
-
-            output_image.at<uchar>(i, j) = sqrt(sum_x * sum_x + sum_y * sum_y);
-        }
-    }
-}
-
-void gauss_sharpness(const Mat& input_image, Mat& output_image, int aperture_size, float sigma, float amount)
-{
-    output_image = input_image.clone();
-    Mat kernel(aperture_size, aperture_size, CV_32F);
-    gauss_kernel(kernel, aperture_size, sigma);
-
-    // Применение фильтра увеличения резкости к изображению  
-    for (int i = aperture_size / 2; i < input_image.rows - aperture_size / 2; i++) {
-        for (int j = aperture_size / 2; j < input_image.cols - aperture_size / 2; j++) {
-
-            float sum_x = 0.0;
-            float sum_y = 0.0;
-
-            for (int x = -aperture_size / 2; x <= aperture_size / 2; x++) {
-                for (int y = -aperture_size / 2; y <= aperture_size / 2; y++) {
-                    sum_x += kernel.at<float>(x + aperture_size / 2, y + aperture_size / 2) * input_image.at<uchar>(i + x, j + y) * x;
-                    sum_y += kernel.at<float>(x + aperture_size / 2, y + aperture_size / 2) * input_image.at<uchar>(i + x, j + y) * y;
-                }
-            }
-
-            float val = sqrt(sum_x * sum_x + sum_y * sum_y);
-            output_image.at<uchar>(i, j) = input_image.at<uchar>(i, j) + amount * (input_image.at<uchar>(i, j) - val);
-        }
-    }
-}
-
-void difference_of_gaussians(const Mat& input_image, Mat& output_image)
-{
-
-    Mat blurred_image3x3(input_image.rows, input_image.cols, CV_8UC1);
-    Mat blurred_image5x5(input_image.rows, input_image.cols, CV_8UC1);
-
-    gauss_bluray(input_image, blurred_image3x3, 3, 1.0);
-    gauss_bluray(input_image, blurred_image5x5, 5, 3.0);
-
-    for (int i = 0; i < input_image.rows; i++)
-    {
-        for (int j = 0; j < input_image.cols; j++)
-        {
-            int diff = blurred_image5x5.at<uchar>(i, j) - blurred_image3x3.at<uchar>(i, j);
-            output_image.at<uchar>(i, j) = abs(diff);
+    for (int y = 0; y < inputImage.rows; ++y) {
+        for (int x = 0; x < inputImage.cols; ++x) {
+            outputImage.at<uchar>(y, x) = cv::saturate_cast<uchar>(
+                blurredImage1.at<uchar>(y, x) - blurredImage2.at<uchar>(y, x)
+            );
         }
     }
 }
@@ -233,7 +217,7 @@ void roberts(const Mat& input_image, Mat& output_image)
 
 
 //сортировка window
-void insertion_sort(int window[])
+void insertionSort(int window[])
 {
     int temp, i, j;
     for (i = 0; i < 9; i++) 
@@ -247,7 +231,7 @@ void insertion_sort(int window[])
 }
 
 // Медианный фильтр 
-void median_blure(Mat& input_image, Mat& output_image) 
+void medianBlure(Mat& input_image, Mat& output_image) 
 {
     int window[9];
 
@@ -271,7 +255,7 @@ void median_blure(Mat& input_image, Mat& output_image)
             window[8] = input_image.at<uchar>(y + 1, x + 1);
 
             // сортируем window, чтобы найти медиану
-            insertion_sort(window);
+            insertionSort(window);
 
             // медиану центральному элементу матрицы
             output_image.at<uchar>(y, x) = window[4];
@@ -296,4 +280,101 @@ void mosaic(const Mat& input_image, Mat& output_image)
             tmp = cv::mean(tmp);
         }
     }
+}
+
+void applyGaussianBlur(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize, double sigma)
+{
+    outputImage = inputImage.clone();
+    int rows = inputImage.rows;
+    int cols = inputImage.cols;
+
+    // Вычисляем половину размера ядра
+    int halfSize = kernelSize / 2;
+
+    // Создаем ядро Гауссиана
+    std::vector<std::vector<double>> gaussianKernel(kernelSize, std::vector<double>(kernelSize, 0.0));
+    double sum = 0.0;
+
+    for (int y = -halfSize; y <= halfSize; ++y) {
+        for (int x = -halfSize; x <= halfSize; ++x) {
+            gaussianKernel[y + halfSize][x + halfSize] = std::exp(-(x * x + y * y) / (2.0 * sigma * sigma));
+            sum += gaussianKernel[y + halfSize][x + halfSize];
+        }
+    }
+
+    // Нормализуем ядро Гауссиана
+    for (int i = 0; i < kernelSize; ++i) {
+        for (int j = 0; j < kernelSize; ++j) {
+            gaussianKernel[i][j] /= sum;
+        }
+    }
+
+    // Применяем фильтр Гаусса
+    for (int y = halfSize; y < rows - halfSize; ++y) {
+        for (int x = halfSize; x < cols - halfSize; ++x) {
+            double sum = 0.0;
+
+            for (int ky = -halfSize; ky <= halfSize; ++ky) {
+                for (int kx = -halfSize; kx <= halfSize; ++kx) {
+                    sum += inputImage.at<uchar>(y + ky, x + kx) * gaussianKernel[ky + halfSize][kx + halfSize];
+                }
+            }
+
+            outputImage.at<uchar>(y, x) = cv::saturate_cast<uchar>(sum);
+        }
+    }
+}
+
+void calculateGradient(const cv::Mat& inputImage, cv::Mat& gradientX, cv::Mat& gradientY)
+{
+    int rows = inputImage.rows;
+    int cols = inputImage.cols;
+
+    // Оператор Собеля для горизонтальной и вертикальной компонент
+    int sobelX[3][3] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1} };
+    int sobelY[3][3] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1} };
+
+    gradientX = cv::Mat::zeros(rows, cols, CV_8U);
+    gradientY = cv::Mat::zeros(rows, cols, CV_8U);
+
+    // Применяем оператор Собеля для подсчета градиента
+    for (int y = 1; y < rows - 1; ++y) {
+        for (int x = 1; x < cols - 1; ++x) {
+            int gx = 0, gy = 0;
+
+            for (int ky = -1; ky <= 1; ++ky) {
+                for (int kx = -1; kx <= 1; ++kx) {
+                    gx += inputImage.at<uchar>(y + ky, x + kx) * sobelX[ky + 1][kx + 1];
+                    gy += inputImage.at<uchar>(y + ky, x + kx) * sobelY[ky + 1][kx + 1];
+                }
+            }
+
+            gradientX.at<uchar>(y, x) = cv::saturate_cast<uchar>(gx);
+            gradientY.at<uchar>(y, x) = cv::saturate_cast<uchar>(gy);
+        }
+    }
+}
+void applyGaussianContour(const cv::Mat& inputImage, cv::Mat& outputImage, int kernelSize, double sigma)
+{
+    // Применяем Гауссово размытие
+    cv::Mat blurredImage;
+    applyGaussianBlur(inputImage, blurredImage, kernelSize, sigma);
+
+    // Вычисляем градиент
+    cv::Mat gradientX, gradientY;
+    calculateGradient(blurredImage, gradientX, gradientY);
+
+    // Уточняем размер выходного изображения
+    outputImage.create(blurredImage.rows, blurredImage.cols, CV_8U);
+
+    // Преобразуем градиентные изображения в тип данных с плавающей точкой
+    cv::Mat gradientXFloat, gradientYFloat;
+    gradientX.convertTo(gradientXFloat, CV_32F);
+    gradientY.convertTo(gradientYFloat, CV_32F);
+
+    // Вычисляем общий градиент
+    cv::magnitude(gradientXFloat, gradientYFloat, outputImage);
+
+    // Нормализуем значения градиента
+    cv::normalize(outputImage, outputImage, 0, 255, cv::NORM_MINMAX, CV_8U);
 }
